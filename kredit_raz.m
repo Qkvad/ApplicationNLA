267e37eab56b5651b985e20a1b43a7b21babe3c4
Petra Brčić <petrabrcic94@gmail.com>
2018-04-03 16:41:36 +0200

    ADD kreditni razred and smeo (masses)

--- kredit_raz.m
+++ kredit_raz.m
@@ -1,9 +1,25 @@
 function kredit_raz (n)
-%n je broj iteracija koje će se izvršiti
+%n is the max number of iterations
 
-load('model_kredit_A.mat')
-u0=zeros(8,1); u0(1)=1;
+load('model_kredit_A.mat')    %matrix
+u0=zeros(8,1); u0(1)=1;       %u0=[1 0 0 0 0 0 0 0]^T
 
+u=A*u0;
+for k=0:n
+  p=plot(1:8,u);
+  title(sprintf("Iteracija broj %d",k));
+  axis([0.5 8.5 0.0 1.0])
+  grid on
+  xlabel('Stanje')
+  ylabel('Gustoća')
+  set(gca,'XTick',1:8)
+  set(gca,'XTickLabel', { 'AAA', 'AA', 'A','BBB', 'BB', 'B', 'CCC', 'D' } );
+  
+  pause(0.2);
+  
+  u=A*u;
+end
 
+[vec,vrij]=eig(A)
 
 end
